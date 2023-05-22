058d31ac04e6352c2b26d39e2d53c83462a68f3c
Georgii <georgii.galushko@gmail.com>
2023-05-22 16:03:48 +0300

    use the input handling from toycalc (TODO: maybe move that stuff to a library)

--- main.c
+++ main.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 
 #include "expr.h"
+#include "input.h"
 
 
 int main()
 {
-	Expr *expr = Lambda_new(
-		Var_new('c'),
-		Appl_new(
-			Lambda_new(Var_new('x'), Var_new('x')),
-			Var_new('y')
-		)
-	);
-	Expr_print(expr);
-	putchar('\n');
+	for (;;) {
+		char *input = get_line();
+		if (!input) {
+			break;
+		}
+		printf("Got input: %s\n", input);
+		Expr *expr = Lambda_new(
+			Var_new('c'),
+			Appl_new(
+				Lambda_new(Var_new('x'), Var_new('x')),
+				Var_new('y')
+			)
+		);
+		Expr_print(expr);
+		Expr_drop(expr);
+		putchar('\n');
+	}
 	return 0;
 }
--- mkfile
+++ mkfile
@@ -1,11 +1,11 @@
 CC=gcc
-CFLAGS=-Wall -Wextra
-SRC=main.c expr.c
+CFLAGS=-g -Wall -Wextra
+SRC=main.c expr.c input.c
 OBJ=${SRC:%.c=%.o}
 PROG=lambda
 
 %.o: %.c
-	$CC $CFLAGS -c $prereq
+	$CC $CFLAGS -c $stem.c
 
 $PROG: $OBJ
 	cc -o $target $prereq
