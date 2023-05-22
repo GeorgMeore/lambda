238e344876c887b7c4730607a37fb34d841f154d
Georgii <>
2023-05-22 15:28:15 +0300

    Initial commit

---
+++ expr.h
@@ -0,0 +1,42 @@
+#ifndef EXPR_INCLUDED
+#define EXPR_INCLUDED
+
+typedef enum {
+	LAMBDA_EXPR,
+	APPL_EXPR,
+	FREE_VAR_EXPR,
+	BOUND_VAR_EXPR,
+} ExprType;
+
+typedef struct Expr Expr;
+
+typedef struct {
+	int arg;
+	Expr *body;
+} LambdaValue;
+
+typedef struct {
+	Expr *left;
+	Expr *right;
+} ApplValue;
+
+typedef union {
+	int var;
+	LambdaValue lambda;
+	ApplValue appl;
+} ExprValue;
+
+struct Expr {
+	ExprType type;
+	ExprValue as;
+};
+
+void Expr_print(Expr *expr);
+
+Expr *Lambda_new(Expr *arg, Expr *body);
+Expr *Appl_new(Expr *left, Expr *right);
+Expr *Var_new(int variable);
+
+void Expr_drop(Expr *expr);
+
+#endif // EXPR_INCLUDED
